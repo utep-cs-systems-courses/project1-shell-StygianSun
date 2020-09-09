726590c9179dcebef5c222f921393fc93ceb8c2a
StygianSun <ramarc@miners.utep.edu>
2020-09-09 13:47:26 -0600

    Added command_check function, began framework for shell functions

--- shell/Shell.py
+++ shell/Shell.py
@@ -2,3 +2,22 @@
 import os, sys, re, time
 
 def check_command(cmd):
+    command = cmd.split()
+    if len(part) > 2:
+        print("cmd check false")
+        return False
+    else:
+        print("cmd check true")
+        return True
+
+run = True
+
+while run:
+    cmd = input("$ ")
+    if cmd == "quit":
+        print("quitting")
+        sys.exit(1)
+    elif cmd == "help":
+        print("help message")
+    else:
+        print("Fork go here")
