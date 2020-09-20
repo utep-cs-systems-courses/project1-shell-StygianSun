a79a2b7fc75ceba8c98e71d6600937baa5cf87f1
StygianSun <ramarc@miners.utep.edu>
2020-09-20 01:21:20 -0600

    Rewrote redirect for input and output, piping, and input validation. Also add testLog.txt to git.

--- shell/Shell.py
+++ shell/Shell.py
@@ -2,104 +2,98 @@
 import os, sys, re, time
 
 def redirect_in(cmd):
-    file_index = cmd.index('<') - 1
-    file_name = cmd[file_index]
-    print(file_name)
-    cmd_index = cmd.index('<') + 1
-    args = cmd[cmd_index:]
-    print(args)
-
+    pid = os.getpid()
     return_code = os.fork()
     if return_code < 0:
         os.write(2, ("Failed to fork, returning").encode())
         sys.exit(1)
     elif return_code == 0:
-        os.close(1)
-        sys.stdout = open(file_name,"w")
-        os.set_inheritable(1,True)
+        os.close(0)
+        os.open(cmd[cmd.index('<')+1], os.O_RDONLY)
+        os.set_inheritable(0,True)
         for dir in re.split(":", os.environ['PATH']):
-            prog = "%s/%s" % (dir, args[0])
+            prog = "%s/%s" % (dir, cmd[0])
             try:
-                os.execve(prog, args, os.environ)
+                os.execve(prog, cmd, os.environ)
             except FileNotFoundError:
                 pass
-        os.write(1, ("Could not run: %s\n" % args[0]).encode())
+        os.write(1, ("Could not run: %s\n" % cmd[0]).encode())
         sys.exit(0)
-
+    else:
+        child_pid = os.wait()
+        
 def redirect_out(cmd):
-    file_index = cmd.index('>') + 1
-    cmd_index = cmd.index('>')
-    file_name = cmd[file_index]
-    args = cmd[:cmd_index]
+    pid = os.getpid()
     return_code = os.fork()
     if return_code < 0:
         os.write(2, ("Failed to fork, returning").encode())
         sys.exit(1)
     elif return_code == 0:
         os.close(1)
-        sys.stdout = open(file_name,'w')
+        os.open(cmd[cmd.index('>')+1], os.O_CREAT | os.O_WRONLY)
         os.set_inheritable(1,True)
 
         for dir in re.split(":", os.environ['PATH']):
-            prog = "%s/%s" % (dir,args[0])
+            prog = "%s/%s" % (dir,cmd[0])
             try:
-                os.execve(prog, args, os.environ)
+                os.execve(prog, cmd, os.environ)
             except FileNotFoundError:
                 pass
-        os.write(1, ("Could not run: %s\n" % args[0]).encode())
+        os.write(1, ("Could not run: %s\n" % cmd[0]).encode())
         sys.exit(0)
-
-def pipe(cmd):
-    pipe = cmd.index('|')
+    else:
+        child_pid = os.wait()
+        
+def pipe(arg):
+    pid = os.getpid()
+    pipe = arg.index('|')
     pr, pw = os.pipe()
-    for fdis in (pr, pw):
-        os.set_inheritable(fdis,True)
+    for fd in (pr, pw):
+        os.set_inheritable(fd,True)
 
     return_code = os.fork()
     if return_code < 0:
         sys.exit(1)
     elif return_code == 0:
-        args = cmd[:pipe]
+        arg = arg[:pipe]
         os.close(1)
         fd = os.dup(pw)
         os.set_inheritable(fd,True)
-        for x in (pr, pw):
-            os.close(x)
-        if os.path.isfile(args[0]):
+        for fd in (pr, pw):
+            os.close(fd)
+
+        for dir in re.split(":", os.environ['PATH']):
+            prog = "%s/%s" % (dir, arg[0])
             try:
-                os.execve(args[0], args, os.environ)
+                os.execve(prog, arg, os.environ)
             except FileNotFoundError:
                 pass
-            else:
-                for dir in re.split(":", os.environ['PATH']):
-                    prog = "%s/%s" % (dir,args[0])
-                    try:
-                        os.execve(prog, args, os.environ)
-                    except FileNotFoundErro:
-                        pass
-                os.write(2, ("Could not run: %s\n" % args[0]).encode())
-                sys.exit(0)
+        os.write(2, ("Could not run: %s\n" % args[0]).encode())
+        sys.exit(0)
+    else:
+        arg = arg[pipe + 1:]
+        os.close(0)
+        fd = os.dup(pr)
+        os.set_inheritable(fd, True)
+        for fd in (pw, pr):
+            os.close(fd)
+        if os.path.isfile(arg[0]):
+            try:
+                os.execve(arg[0], arg, os.environ)
+            except FileNotFoundError:
+                pass
         else:
-            args = cmd[pipe + 1:]
-            os.close(0)
-            fd = os.dup(pr)
-            for fd in (pw, pr):
-                os.close(fd)
-            if os.path.isfile(args[0]):
+            for dir in re.split(":", os.environ['PATH']):
+                prog = "%s/%s" % (dir,arg[0])
                 try:
-                    os.execve(args[0], args, os.environ)
+                    os.execve(prog, arg, os.environ)
                 except FileNotFoundError:
                     pass
-            else:
-                for dir in re.split(":", os.environ['PATH']):
-                    prog = "%s/%s" % (dir,args[0])
-                    try:
-                        os.execve(prog, args, os.environ)
-                    except FileNotFoundError:
-                        pass
-                os.write(2, ("%s isn't a recognized command" % args[0]).encode())
+            os.write(2, ("%s isn't a recognized command" % args[0]).encode())
+            sys.exit(1)
 
 def run(cmd):
+    pid = os.getpid()
     return_code = os.fork()
     if return_code < 0:
         os.write(2,("Failed to fork").encode())
@@ -111,34 +105,45 @@ def run(cmd):
                 os.execve(prog, cmd, os.environ)
             except FileNotFoundError:
                 pass
-        os.write(2, ("%s isn't a recognized command\n" % cmd[0]).encode())
+        os.write(2, ("If '%s' is not a typo, use:\n     cnf %s\n" % (cmd[0],cmd[0])).encode())
         sys.exit(0)
-        
-print("Please enter a command. 'quit' exits the shell.") 
+    else:
+        child_pid = os.wait()
 
+def input_handler(cmd):
+    cmd = cmd.split()
+    if 'quit' in cmd:
+        sys.exit(1)
+    elif not cmd:
+        pass
+    elif '<' in cmd:
+        redirect_in(cmd)
+    elif '>' in cmd:
+        redirect_out(cmd)
+    elif '|' in cmd:
+        pipe(cmd)
+    elif '&' in cmd:
+        cmd.remove("&")
+        wait = False
+    elif 'cd' in cmd:
+        directory = cmd[1]
+        try:
+            os.chdir(directory)
+        except FileNotFoundError:
+            os.write(2,("File: %s not found" % directory).encode())
+    else:
+        run(cmd)
 
-##Main runtime loop
 while True:
-    print("$", end=" ")
     if 'PS1' in os.environ:
-        os.write(1, os.environ['PS1'].encode())
+        os.write(1,(os.environ['PS1']).encode())
+    else:
+        os.write(1,("$ ").encode())
     try:
-        user_input = input()
+        cmd = input()
     except EOFError:
         sys.exit(1)
     except ValueError:
         sys.exit(1)
-    cmd = user_input.split()
 
-    if not cmd: ##If entry is blank, prompts for user input
-        print("Please enter a command")
-    elif "quit" in cmd: ##Exits primary shell
-        sys.exit(0)
-    elif "<" in cmd: ## Check for an input redirect
-        redirect_in(cmd)
-    elif ">" in cmd: ## Check for an output redirect
-        redirect_out(cmd)
-    elif "|" in cmd: ## Check for pipe command
-        pipe(cmd)
-    else: ## Runs any remaining valid command
-        run(cmd)
+    input_handler(cmd)
